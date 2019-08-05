300956956ff712364c4b7df5017a136701cee3f4
Singo Xuan <singohuen@gmail.com>
2019-08-05 21:58:36 +1000

    Replace Graphql URI with env var

--- web/lib/withApollo.js
+++ web/lib/withApollo.js
@@ -4,7 +4,7 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 export default withApollo(
   ({ ctx, headers, initialState }) =>
     new ApolloClient({
-      uri: 'https://graphql-dot-fifth-inkwell-181305.appspot.com/graphql',
+      uri: process.env.GRAPHQL_URI,
       cache: new InMemoryCache().restore(initialState || {})
     })
 );
